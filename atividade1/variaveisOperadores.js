3849c6891057e380de75d1a4ac1938c2bbf2292f
MARCELLO ROMERO DAS NEVES JUMIOR <marcello.jumior@a.ucb.br>
2023-08-10 10:40:21 -0300

    atividade1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,12 @@
+var numero1 = 2;
+var numero2 = 3;
+var soma = numero1 + numero2;
+var sub = numero1 - numero2;
+var div = numero1 / numero2;
+var mult = numero1 * numero2;
+
+console.log("Resultado da soma: " +soma)
+console.log("Resultado da subtração: " +sub)
+console.log("Resultado da divisão: " + div)
+console.log("Resultado da multiplicação: " + mult)
+
